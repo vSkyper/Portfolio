e23e59d6c444e9f9efd5f37edf2fe05b11426543
Mateusz Piwowarski <matpiw1@st.amu.edu.pl>
2023-06-05 17:45:00 +0200

    Refactor code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { HelloHandwriting } from 'components/Home';
 export default function Home() {
   return (
     <main className='container mx-auto w-11/12 h-full'>
-      <div className='w-full h-full flex flex-col items-center justify-center'>
+      <div className='w-full h-full flex items-center justify-center'>
         <HelloHandwriting />
       </div>
     </main>
