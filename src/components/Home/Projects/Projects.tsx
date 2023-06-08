52fcf43cd46f5a031482356c6a2c6ae10028ad53
Mateusz Piwowarski <matpiw1@st.amu.edu.pl>
2023-06-08 11:46:21 +0200

    Repair slider resizing

--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -5,20 +5,53 @@ import { Cards } from './components';
 import { useEffect, useRef, useState } from 'react';
 
 export default function Projects() {
-  const [width, setWidth] = useState<number>(0);
-  const carousel = useRef<HTMLDivElement>(null);
+  const [offset, setOffset] = useState<number>(0);
+  const [dragField, setDragField] = useState<number>(0);
+
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const dragFieldRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!carousel.current) return;
+    const updateOffset = () => {
+      if (wrapperRef.current && contentRef.current) {
+        const { width } = wrapperRef.current.getBoundingClientRect();
 
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+        const offSetWidth = contentRef.current.scrollWidth;
+        const newOffset = offSetWidth - width;
+
+        setTimeout(() => {
+          setOffset(newOffset);
+          setDragField(offSetWidth);
+        }, 500);
+      }
+    };
+
+    updateOffset();
+
+    window.addEventListener('resize', updateOffset);
+    return () => {
+      window.removeEventListener('resize', updateOffset);
+    };
   }, []);
 
   return (
-    <div ref={carousel} className='pt-3 sm:pt-9 lg:pt-20 xl:pt-28 mx-2 sm:mx-6'>
+    <div
+      ref={wrapperRef}
+      className='relative pt-3 sm:pt-9 lg:pt-20 xl:pt-28 mx-2 sm:mx-6'
+    >
+      <div
+        ref={dragFieldRef}
+        className='absolute inset-0 pointer-events-none'
+        style={{
+          left: `-${offset}px`,
+          width: `${dragField}px`,
+        }}
+      ></div>
       <motion.div
-        drag='x'
-        dragConstraints={{ right: 0, left: -width }}
+        ref={contentRef}
+        drag={'x'}
+        dragConstraints={dragFieldRef}
         whileTap={{ cursor: 'grabbing' }}
         className='flex gap-2 sm:gap-6 cursor-grab'
       >
